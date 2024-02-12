1ffed88b6c589cd078a79e2d4e8295273c3166ef
Mack Adrian Bautista <113152526+mackadrian@users.noreply.github.com>
2024-02-12 13:38:54 -0700

    changed game to work on 32x32 bitmaps instead

--- BMBER/stage_2/RASTER.C
+++ BMBER/stage_2/RASTER.C
@@ -6,14 +6,14 @@
 /*
 ----- FUNCTION: clear_screen -----
 Purpose: clears the screen
-Parameters: base (unsigned short frame buffer)
+Parameters: base (unsigned long frame buffer)
 */
-void clear_screen(UINT16 *base)
+void clear_screen(UINT32 *base)
 {
-	UINT16 *end = base + (SCREEN_WIDTH * SCREEN_HEIGHT);
+	UINT32 *end = base + ((SCREEN_WIDTH * SCREEN_HEIGHT) / 32);
 	while (base < end)
 	{
-		*base++ = 0x0000;
+		*base++ = 0x00000000;
 	}
 }
 
@@ -21,14 +21,14 @@ void clear_screen(UINT16 *base)
 ----- FUNCTION: plot_vline -----
 Purpose: plots a vertical line in the given y1 and x position.
 		y1 to y2 plots the length of the vertical line.
-Parameters: base (unsigned short frame buffer),
+Parameters: base (unsigned long frame buffer),
 			x (horizontal position),
 			y1 (beginning length of the vert. line),
 			y2 (ending length of the vert. line)
 */
-void plot_vline(UINT16 *base, int x, int y1, int y2)
+void plot_vline(UINT32 *base, int x, int y1, int y2)
 {
-	UINT16 *loc = base + (y1 * 40) + (x >> 4);
+	UINT32 *loc = base + (y1 * 20);
 	int i, length;
 
 	if (y1 >= SCREEN_HEIGHT || y2 >= SCREEN_HEIGHT || x >= SCREEN_WIDTH)
@@ -46,7 +46,7 @@ void plot_vline(UINT16 *base, int x, int y1, int y2)
 	length = y2 - y1;
 	for (i = 0; i < length; i++)
 	{
-		*(loc + (y1 + i) * 40 + (x >> 4)) |= 1 << 7 - ((x & 7));
+		*(loc + (y1 + i) * 20 + (x >> 5)) |= 0xFFFFFFFF;
 	}
 }
 
@@ -54,14 +54,14 @@ void plot_vline(UINT16 *base, int x, int y1, int y2)
 ----- FUNCTION: plot_hline -----
 Purpose: plots a horizontal line in the given x1 and y position.
 	 x1 to x2 plots the length of the horizontal line.
-Parameters:	base (unsigned short frame buffer),
+Parameters:	base (unsigned long frame buffer),
 			x1 (beginning length of the hor. line),
 			y (vertical position),
 			x2 (ending length of the hor. line)
 */
-void plot_hline(UINT16 *base, int x1, int y, int x2)
+void plot_hline(UINT32 *base, int x1, int y, int x2)
 {
-	UINT16 *loc = base + (y * 40) + (x1 >> 4);
+	UINT32 *loc = base + (y * 20) + (x1 >> 5);
 	int i, length, l_shift, r_shift;
 
 	if (x1 >= SCREEN_WIDTH || x2 >= SCREEN_WIDTH || y >= SCREEN_HEIGHT)
@@ -77,40 +77,40 @@ void plot_hline(UINT16 *base, int x1, int y, int x2)
 	}
 
 	length = x2 - x1;
-	l_shift = x1 % 16;
-	r_shift = (16 - ((length - (16 - l_shift)) % 16));
+	l_shift = x1 % 32;
+	r_shift = (32 - ((length - (32 - l_shift)) % 32));
 
 	if (l_shift > 0)
 	{
-		*loc |= 0xFFFF >> l_shift;
+		*loc |= 0xFFFFFFFFF >> l_shift;
 		loc++;
 	}
 
-	for (i = 0; i < ((length - l_shift) >> 4) && (loc < (base + ((y + 1) * 40))); i++)
+	for (i = 0; i < ((length - l_shift) >> 5) && (loc < (base + ((y + 1) * 20))); i++)
 	{
-		*loc++ = 0xFFFF;
+		*loc++ = 0xFFFFFFFFF;
 	}
 
-	if (loc < base + ((y + 1) * 40) && r_shift > 0)
+	if (loc < base + ((y + 1) * 20) && r_shift > 0)
 	{
-		*loc |= 0xFFFF << r_shift;
+		*loc |= 0xFFFFFFFF << r_shift;
 	}
 }
 
 /*
 ----- FUNCTION: plot_bitmap16 -----
 Purpose: To plot out a 16x16 bitmap array into the screen.
-Parameters:	base (unsigned short frame buffer),
+Parameters:	base (unsigned long frame buffer),
 			x (horizontal coordinate),
 			y (vertical coordinate).
 			bitmap (unsigned short bitmap array),
 			height (height of the bitmap),
 			width (width of the bitmap)
 */
-void plot_bitmap16(UINT16 *base, int x, int y, const UINT16 *bitmap,
+void plot_bitmap16(UINT32 *base, int x, int y, const UINT32 *bitmap,
 				   unsigned int height)
 {
-	UINT16 *loc = base + (y * 40) + (x >> 4);
+	UINT32 *loc = base + (y * 20) + (x >> 5);
 	int i, shift;
 
 	if (x <= SCREEN_WIDTH || y <= SCREEN_HEIGHT)
@@ -118,20 +118,20 @@ void plot_bitmap16(UINT16 *base, int x, int y, const UINT16 *bitmap,
 		return;
 	}
 
-	shift = x % 16;
+	shift = x % 32;
 
 	if (shift > 0)
 	{
 		for (i = 0; i < SCREEN_HEIGHT && loc < base; i++)
 		{
 			*loc |= bitmap[i] >> shift;
-			loc += 40;
+			loc += 20;
 		}
-		loc = loc - (40 * SCREEN_HEIGHT) + 1;
+		loc = loc - (20 * SCREEN_HEIGHT) + 1;
 		for (i = 0; i < SCREEN_HEIGHT && loc < base; i++)
 		{
-			*loc = (bitmap[i] << 16 - shift);
-			loc += 40;
+			*loc = (bitmap[i] << 32 - shift);
+			loc += 20;
 		}
 	}
 	else
@@ -139,7 +139,7 @@ void plot_bitmap16(UINT16 *base, int x, int y, const UINT16 *bitmap,
 		for (i = 0; i < SCREEN_HEIGHT; i++)
 		{
 			*loc |= bitmap[i];
-			loc += 40;
+			loc += 20;
 		}
 	}
 }
