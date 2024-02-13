c26253859b38ce8d0cde6bbda7209df62a8774d7
Mack Adrian Bautista <113152526+mackadrian@users.noreply.github.com>
2024-02-12 22:12:02 -0700

    implement thickness for plot_hline/bitmaps

---
+++ BMBER/stage_2/RASTER.C
@@ -0,0 +1,128 @@
+#include "raster.h"
+
+#define SCREEN_WIDTH 640
+#define SCREEN_HEIGHT 400
+
+/*
+----- FUNCTION: clear_screen -----
+Purpose: clears the screen
+Parameters: base (unsigned long frame buffer)
+*/
+void clear_screen(UINT32 *base)
+{
+	UINT32 *end = base + ((SCREEN_WIDTH * SCREEN_HEIGHT) / 32);
+	while (base < end)
+	{
+		*base++ = 0x00000000;
+	}
+}
+
+/*
+----- FUNCTION: plot_vline -----
+Purpose: plots a vertical line in the given y1 and x position.
+		y1 to y2 plots the length of the vertical line.
+Parameters: base (unsigned long frame buffer),
+			x (horizontal position),
+			y1 (beginning length of the vert. line),
+			y2 (ending length of the vert. line)
+*/
+void plot_vline(UINT32 *base, int x, int y, int length)
+{
+	int i;
+
+	if (x >= SCREEN_WIDTH || y >= SCREEN_HEIGHT || length >= SCREEN_HEIGHT)
+	{
+		return;
+	}
+
+	for (i = 0; i < length; i++)
+	{
+		*(base + (y + i) * 20 + (x >> 5)) |= 0xFFFFFFFF;
+	}
+}
+
+/*
+----- FUNCTION: plot_hline -----
+Purpose: plots a horizontal line in the given x1 and y position.
+	 x1 to x2 plots the length of the horizontal line.
+Parameters:	base (unsigned long frame buffer),
+			x1 (beginning length of the hor. line),
+			y (vertical position),
+			x2 (ending length of the hor. line)
+*/
+void plot_hline(UINT32 *base, int x, int y, int length)
+{
+	UINT32 *loc = base + (y * 20) + (x >> 5);
+	int i, j, l_shift, r_shift;
+
+	if (x >= SCREEN_WIDTH || y >= SCREEN_HEIGHT || length >= SCREEN_WIDTH)
+	{
+		return;
+	}
+
+	l_shift = x % 32;
+	r_shift = (32 - ((length - (32 - l_shift)) % 32));
+
+	if (l_shift > 0)
+	{
+		*loc |= 0xFFFFFFFFF >> l_shift;
+		loc++;
+	}
+
+	for (i = 0; i < ((length - l_shift) >> 5) && (loc < (base + ((y + 1) * 20))); i++)
+	{
+		*loc++ = 0xFFFFFFFFF;
+	}
+
+	if (loc < base + ((y + 1) * 20) && r_shift > 0)
+	{
+		*loc |= 0xFFFFFFFF << r_shift;
+	}
+}
+
+/*
+----- FUNCTION: plot_bitmap32 -----
+Purpose: To plot out a 32x32 bitmap array into the screen.
+Parameters:	base (unsigned long frame buffer),
+			x (horizontal coordinate),
+			y (vertical coordinate).
+			bitmap (unsigned short bitmap array),
+			height (height of the bitmap),
+			width (width of the bitmap)
+*/
+void plot_bitmap32(UINT32 *base, int x, int y, const UINT32 *bitmap,
+				   unsigned int height)
+{
+	UINT32 *loc = base + (y * 20) + (x >> 5);
+	int i, shift;
+
+	if (x >= SCREEN_WIDTH || y >= SCREEN_HEIGHT)
+	{
+		return;
+	}
+
+	shift = x % 32;
+
+	if (shift > 0)
+	{
+		for (i = 0; i < SCREEN_HEIGHT && loc < base; i++)
+		{
+			*loc |= bitmap[i] >> shift;
+			loc += 20;
+		}
+		loc = loc - (20 * SCREEN_HEIGHT) + 1;
+		for (i = 0; i < SCREEN_HEIGHT && loc < base; i++)
+		{
+			*loc = (bitmap[i] << 32 - shift);
+			loc += 20;
+		}
+	}
+	else
+	{
+		for (i = 0; i < SCREEN_HEIGHT; i++)
+		{
+			*loc |= bitmap[i];
+			loc += 20;
+		}
+	}
+}
